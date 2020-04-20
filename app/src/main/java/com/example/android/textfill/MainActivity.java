e6c208beea12f850a6823922b4333469ed0c1198
Ankita13-code <ankitapareek2000@gmail.com>
2020-04-20 23:29:27 +0530

    gyroscope also implemented

--- app/src/main/java/com/example/android/textfill/MainActivity.java
+++ app/src/main/java/com/example/android/textfill/MainActivity.java
@@ -1,16 +1,42 @@
 package com.example.android.textfill;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-import com.example.android.textfilllibrary.TextFill;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.gjiazhe.panoramaimageview.GyroscopeObserver;
+import com.gjiazhe.panoramaimageview.PanoramaImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+private PanoramaImageView panoramaImageView;
+    private GyroscopeObserver gyroscopeObserver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        gyroscopeObserver = new GyroscopeObserver();
+        gyroscopeObserver.setMaxRotateRadian(Math.PI/2);
+
+        panoramaImageView = findViewById(R.id.panorama_image_view);
+        panoramaImageView.setGyroscopeObserver(gyroscopeObserver);
+
     }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        gyroscopeObserver.register(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        gyroscopeObserver.unregister();
+    }
+
+
 }
